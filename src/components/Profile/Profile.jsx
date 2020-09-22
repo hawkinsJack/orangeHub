535abff2574c795ba4b7604afef4c5892f9a096e
Imad Ahmed <ahmed8@science.regn.net>
2020-09-22 13:50:11 +0100

    Collapse feature added as well as magin to card

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Profile = ({ name, location, role, bio }) => {
-const [open, setOpen]=useState(true)
-const handleOpen = ()=>{
-    console.log(bio)
-setOpen(!open)
+    const [open, setOpen] = useState(false)
 
-}
-
+    const handleOpen = () => {
+        setOpen(!open)
+    }
 
     return (
-        <div className="card w-50">
+        <div className="card w-auto mx-5 my-5 ">
             <div className="card-body">
                 <h3 className="profile-name card-title">{name}</h3>
 
                 <h4 className="profile-location card-subtitle mb-2 text-muted">{location}</h4>
-            
+
                 <h4 className="profile-role">{role}</h4>
-            <button onClick={handleOpen}>Bio</button>
+
+                <button onClick={handleOpen} type="button" class="btn btn-warning">Expand for Bio</button>
+
                 {open && <p className="profile-bio">{bio}</p>}
             </div>
         </div>
