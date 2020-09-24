d7eda7235b2999e5077ff085fb6a4555538eabf3
sarinaamin <s.amin@elsevier.com>
2020-09-24 14:28:37 +0100

    changes to css for cards

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -14,13 +14,8 @@ const Profile = ({ name, location, role, bio, year }) => {
                 <h4 className="profile-location card-subtitle mb-2 text-muted">{location}</h4>
                 <h4 className="profile-role">{role}</h4>
                 <h4 className="profile-role">{year}</h4>
-
-                {
-                    bio &&
-                    <button onClick={handleOpen} type="button" class="btn btn-warning">{!open ? 'Expand for Bio' : 'close'}</button>
-                }
-
-                {open && <p className="profile-bio pt-3">{bio}</p>}
+                <button onClick={handleOpen} type="button" class="btn btn-warning">{!open ? 'Expand for Bio' : 'Close Bio'}</button>
+                {open && <p className="profile-bio pt-3">{bio ? bio : "No Bio Available"}</p>}
             </div>
         </div>
     )
--- src/components/ProfileList/ProfileList.jsx
+++ src/components/ProfileList/ProfileList.jsx
@@ -81,44 +81,61 @@ const ProfileList = ({ listTitle, profiles }) => {
             <img src="/elsevierlogo.png" class="img-fluid mb-5 px-5" alt="Elsevier Logo" />
             <hr style={{ width: '75%', textAlign: 'center', }} />
             <h2>{listTitle ? listTitle : 'Cohort Profiles'}</h2>
-            <input type="text" name="searchName" value={search} onChange={(e) => handleUserInput(e)} />
-            <select value={location} onChange={(e) => handleLocationChange(e)}>
-                <option value=''>All</option>
-                {
-                    Locations.map(location => (
 
-                        <option value={location}>{location}</option>))
-                }
-            </select>
-            <select value={role} onChange={(e) => handleRoleChange(e)} >
-                <option value=''>All</option>
-                {
-                    Roles.map((r) => (
+            
+            <div className="form-row m-5">
+                <div className="col-sm-12 col-md-12">
+                    <input className="form-control mb-2" type="text" name="searchName" placeholder="Search by Name..." value={search} onChange={(e) => handleUserInput(e)} />
+                </div>
+                <div className="col-sm-12 col-md-4">
+                    <select className="form-control mb-2" value={location} onChange={(e) => handleLocationChange(e)}>
+                        <option value="" disabled selected hidden>Select Location...</option>
+                        <option value=''>All</option>
+                        {
+                            Locations.map(location => (
 
-                        <option value={r}>{r}</option>
-                    ))
-                }
-            </select>
-            <select value={year} onChange={(e) => handleYearChange(e)}>
-                <option value=''>All</option>
-                {
-                    Years.map((y) => (
+                                <option value={location}>{location}</option>))
+                        }
+                    </select>
+                </div>
+                <div className="col-sm-12 col-md-4">
+                    <select className="form-control mb-2" value={role} onChange={(e) => handleRoleChange(e)} >
+                        <option value="" disabled selected hidden>Select Role...</option>
+                        <option value=''>All</option>
+                        {
+                            Roles.map((r) => (
+                                <option value={r}>{r}</option>
+                            ))
+                        }
+                    </select>
+                </div>
+                <div className="col-sm-12 col-md-4 mb-sm-3 mb-md-0">
+                    <select className="form-control" value={year} onChange={(e) => handleYearChange(e)}>
+                        <option value="" disabled selected hidden>Select Year...</option>
+                        <option value=''>All</option>
+                        {
+                            Years.map((y) => (
 
-                        <option value={y}>{y}</option>
-                    ))
-                }
-            </select>
+                                <option value={y}>{y}</option>
+                            ))
+                        }
+                    </select>
+                </div>
+            </div>
+
             <div class="container">
-                <div class="row">
-                    {
-                        filteredProfiles &&
-                        filteredProfiles.length !== 0 &&
-                        filteredProfiles.filter((profile) => (lowerCaseAndRemoveWhitespace(profile.name).includes(lowerCaseAndRemoveWhitespace(search)))).map((profile) => (
-                            <div class="col-md-6 col-lg-4">
-                                <Profile name={profile.name} location={profile.location} role={profile.role} bio={profile.bio} year={profile.year} />
-                            </div>
-                        ))
-                    }
+                <div className="card-deck md-sm-3">
+                    <div class="row">
+                        {
+                            filteredProfiles &&
+                            filteredProfiles.length !== 0 &&
+                            filteredProfiles.filter((profile) => (lowerCaseAndRemoveWhitespace(profile.name).includes(lowerCaseAndRemoveWhitespace(search)))).map((profile) => (
+                                <div class="col-md-6 col-lg-4 ">
+                                    <Profile name={profile.name} location={profile.location} role={profile.role} bio={profile.bio} year={profile.year} />
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
